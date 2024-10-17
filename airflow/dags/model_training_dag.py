1d9fe6888ff52e4a4b4ea32d5a87de2dd7a61829
bilalimamoglu <xfyzal@gmail.com>
2024-10-17 22:22:02 +0200

    Add Docker integration for Airflow and model training
    Enhanced the environment to use Docker for running Airflow tasks and model training. Added necessary packages, updated Docker-related configurations, and integrated DockerOperator for model training in the DAG. Included new Dockerfiles for various components to streamline dependency management and execution.

--- airflow/dags/model_training_dag.py
+++ airflow/dags/model_training_dag.py
@@ -1,5 +1,5 @@
 from airflow import DAG
-from airflow.operators.bash import BashOperator
+from airflow.providers.docker.operators.docker import DockerOperator
 from airflow.utils.dates import days_ago
 from datetime import timedelta
 import os
@@ -17,16 +17,23 @@ default_args = {
 with DAG(
     'daily_model_training',
     default_args=default_args,
-    description='Train and log ML model daily',
+    description='Train and log ML model daily using Docker',
     schedule_interval=timedelta(days=1),
     start_date=days_ago(1),
     catchup=False,
 ) as dag:
 
-    train_model = BashOperator(
+    train_model = DockerOperator(
         task_id='train_model',
-        bash_command='source ~/Projects/ml-lifecycle-system/myenv/bin/activate && python ~/Projects/ml-lifecycle-system/training/model_training.py',
-        env={
+        image='ml-lifecycle-training:latest',  # Ensure this matches your training image
+        api_version='auto',
+        auto_remove=True,
+        command="python model_training.py",
+        docker_url="unix://var/run/docker.sock",  # Default Docker URL
+        network_mode="host",  # Use host network to allow container to access Mlflow server
+        environment={
             'MLFLOW_TRACKING_URI': 'http://mlflow-server:5000',
+            'AWS_ACCESS_KEY_ID': os.getenv('AWS_ACCESS_KEY_ID'),
+            'AWS_SECRET_ACCESS_KEY': os.getenv('AWS_SECRET_ACCESS_KEY'),
         },
     )
